3f8233c9079ad1e00277d1e295972325ee389e99
Han Min <minhan0821@outlook.com>
2023-12-10 19:56:47 -0800

    bug fixed

--- src/Shopping/users/signin.js
+++ src/Shopping/users/signin.js
@@ -1,16 +1,17 @@
 import * as client from "./client";
-import { useState, useEffect} from "react";
-import { useNavigate, Link } from "react-router-dom";
+import {useState, useEffect} from "react";
+import {useNavigate, Link} from "react-router-dom";
 import './sign.css'
-function Signin({ setIsAuthenticated }) {
+
+function Signin({setIsAuthenticated}) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const navigate = useNavigate();
-  
+
   const signIn = async () => {
     try {
-      const credentials = { username, password };
+      const credentials = {username, password};
       const user = await client.signin(credentials);
       if (user) {
         setIsAuthenticated(true); // Set authenticated state to true
@@ -26,41 +27,39 @@ function Signin({ setIsAuthenticated }) {
     }
   };
   return (
-    <div class="tab-content mt-5">
-      <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-        <form onSubmit={signIn}>
+      <div class="tab-content mt-5">
+        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
           {error && <div className="error">{error}</div>}
           <h3>Sign In</h3>
           <div class="form-outline mb-4">
-            <input 
-              type="text"
-              id="loginName"
-              class="form-control"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+            <input
+                type="text"
+                id="loginName"
+                class="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
             />
             <label class="form-label" for="loginName">Username</label>
           </div>
 
           <div class="form-outline mb-4">
-            <input 
-              type="password"
-              id="loginPassword"
-              class="form-control"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+            <input
+                type="password"
+                id="loginPassword"
+                class="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
             <label class="form-label" for="loginPassword">Password</label>
           </div>
 
-          <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+          <button type="submit" onClick={signIn} class="btn btn-primary btn-block mb-4">Sign in</button>
 
           <div>
-            <p>Not a member?<Link to = "/shopping/signup">Signup</Link></p>
+            <p>Not a member?<Link to="/shopping/signup">Signup</Link></p>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
   );
 }
 
